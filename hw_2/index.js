ababa898e1a4c1bdab356a8c22fc235c212c2f93
SeriousDuck <zotz321@gmail.com>
2025-02-07 23:51:59 +0200

    End hw_2,3,4

---
+++ hw_2/index.js
@@ -0,0 +1,34 @@
+//? hw-2
+// 1 
+// const firstWord = prompt('Write first words');
+// const secondWord = prompt('Write second words');
+// const thirdWord = prompt('Write third words');
+
+// console.log(`${thirdWord}, ${firstWord}, ${secondWord}`);
+
+// 2
+// const inputNum = prompt('Write five-digit number');
+// console.log(inputNum.split('').join(' '));
+
+
+//? hw_3
+// const num1 = prompt("First number");
+// const num2 = prompt("Second number");
+
+// var plusSum = parseInt(num1) + parseInt(num2);
+// var minusSum = num1 - num2;
+// var multiplicationSum = num1 * num2;
+// var divisionSum = num1 / num2;
+
+// console.log(`Користувач ввів ${num1} і ${num2}: 
+//     ${num1} + ${num2} = ${plusSum},
+//     ${num1} - ${num2} = ${minusSum},
+//     ${num1} / ${num2} = ${divisionSum}, 
+//     ${num1} * ${num2} = ${multiplicationSum}
+// `);
+
+//? hw_4
+const second = 3600;
+const numberHours = prompt("Enter a number of hours");
+const sum = numberHours * second;
+alert(sum);
